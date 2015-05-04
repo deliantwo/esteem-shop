6dd4900cdda57a2a36d966a61a4550774a37d0d1
wojzag <eivelloth@gmail.com>
2015-05-04 12:37:08 +0000

    Some minor config files for Rails Admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,5 +1,14 @@
 RailsAdmin.config do |config|
 
+  # app name
+  config.main_app_name = ["eSteem", "Admin Panel"]
+  
+  # Devise
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
+
   ### Popular gems integration
 
   ## == Devise ==
