6c9e2057b0596b2f5997837be8f5fd4df18dd208
wojzag <eivelloth@gmail.com>
2015-05-11 18:43:47 +0000

    Added tokens to cart operations

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,8 +2,14 @@ class CartController < ApplicationController
     before_filter :authenticate_user!, only: :checkout
     
     def detail_view
+        if @shopping_cart.total_unique_items == 0
+            redirect_to root_path
+        end
     end
     
     def checkout
+        if @shopping_cart.total_unique_items == 0
+            redirect_to root_path
+        end
     end
 end
