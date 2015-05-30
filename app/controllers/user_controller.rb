e4adc0ca04fe47700137453ddfa1058e1fdc9776
VallThore <vallthore@gmail.com>
2015-05-30 12:53:50 +0000

    Fixed transaction history summary - many products are now shown as one

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -12,15 +12,18 @@ class UserController < ApplicationController
     all_purchaces.each do |p|
       sp_list = SoldProduct.where(purchase: p)
       
-      sp_to_add = []
+      sp_to_add = Hash.new
       sp_list.each do |sp|
         gamekey = Gamekey.find(sp.gamekey_id)
         vat = Vat.find(sp.vat_id)
         ppg = PricePlatformGame.find(gamekey.price_platform_game_id)
         platform = Platform.find(ppg.platform_id)
         game = Game.find(ppg.game_id)
-        
-        sp_to_add.push([sp, game, platform, vat])
+        if(sp_to_add[ppg].nil?)
+          sp_to_add[ppg] = [1, sp, game, platform, vat]
+        else
+          sp_to_add[ppg][0] += 1
+        end
       end
       @purchases.push([p, sp_to_add])
     end
