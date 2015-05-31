4b1b32726a86c53fa3108b777e6a170fbeef0441
VallThore <vallthore@gmail.com>
2015-05-31 11:13:29 +0000

    Some db fixes with

--- db/migrate/20150408154021_create_sold_products.rb.rb
+++ db/migrate/20150408154021_create_sold_products.rb.rb
@@ -1,10 +1,10 @@
 class CreateSoldProducts < ActiveRecord::Migration
   def change
     create_table :sold_products do |t|
-      t.integer :purchase_id
-      t.integer :gamekey_id
-      t.integer :vat_id
-      t.decimal :price
+      t.integer :purchase_id, default: false, null: false
+      t.integer :gamekey_id, default: false, null: false
+      t.integer :vat_id, default: false, null: false
+      t.decimal :price, default: false, null: false
 
       t.timestamps null: false
     end
--- db/migrate/20150408155033_create_vats.rb
+++ db/migrate/20150408155033_create_vats.rb
@@ -1,7 +1,7 @@
 class CreateVats < ActiveRecord::Migration
   def change
     create_table :vats do |t|
-      t.decimal :value
+      t.decimal :value, default: false, null: false
 
       t.timestamps null: false
     end
